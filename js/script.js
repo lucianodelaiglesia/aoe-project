4d646258de1b243feda1f9151d99e4e7ed3c90c2
Luciano de la Iglesia <l.delaiglesia@hotmail.com>
2021-02-17 18:30:00 -0300

    media querys

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("script");
+function logoSize() {
+  size = window.screen.width;
+  logo = document.getElementById("logo");
+  if (size <= 600) {
+    logo.src = "../img/aoe_logo_mobile.png";
+  } else {
+    logo.src = "../img/aoe_logo_complete.png";
+  }
+}
+
+setInterval("logoSize()", 100);
